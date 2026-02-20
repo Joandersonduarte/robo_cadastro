130f0ec39758cd4b19ec3715435da753775a4d62
Joandersonduarte <joandersonduarte13@gmail.com>
2026-02-19 22:46:24 -0300

    Finalização do bot, incluido o fluxo cadastro dos dados no site.

--- projeto/bot.py
+++ projeto/bot.py
@@ -19,14 +19,55 @@ servico = Service(ChromeDriverManager().install())
 navegador = w.Chrome(service=servico, options=chrome_options)
 wait = WebDriverWait(navegador, 5)
 
+
+def leitura_dados():
+     try:
+          df = pd.read_excel('/home/joanderson/Área de trabalho/Projetos/robo_cadastro/challenge.xlsx')
+          print('Dados lidos com sucesso.')
+          return df
+     except FileNotFoundError:
+          print('Arquivo não encontrado.')
+          return None
+
 def login():
     try:
         navegador.get('https://rpachallenge.com/')
-        print('Acessando o site...')
+        navegador.maximize_window()
+        print('Acessando site.')
+    except TimeoutError:
+            navegador.refresh()
+            print('Tempo de carregamento excedido. Tentando novamente...')
     except Exception as e:
-        print('Erro ao acessar o site:', e)
-        return False
+        navegador.quit()
+        print('Ocorreu um erro inesperado: ', e)
 
+def start_boot():
+    dados = leitura_dados()
+    
+    for index, row in dados.iterrows():
+        nome = row['First Name']
+        sobrenome = row['Last Name ']
+        companhia = row['Company Name']
+        cargo = row['Role in Company']
+        endereco = row['Address']
+        email = row['Email']
+        fone = row['Phone Number']
 
-if __name__ == "__main__":
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelRole"]'))).send_keys(cargo)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelFirstName"]'))).send_keys(nome)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelCompanyName"]'))).send_keys(companhia)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelAddress"]'))).send_keys(endereco)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelEmail"]'))).send_keys(email)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelPhone"]'))).send_keys(fone)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[ng-reflect-name="labelLastName"]'))).send_keys(sobrenome) 
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'input[type="submit"]'))).click() 
+        print(f'Cadastro de {nome} {sobrenome} realizado com sucesso.')
+    print('Todos os cadastros foram realizados com sucesso.')
+    t.sleep(5)
+
+if __name__ == '__main__':
+    print('Bot iniciando às: ', dt.datetime.now().strftime('%H:%M:%S'))
     login()
+    print('Iniciando cadastro dos usuários às: ', dt.datetime.now().strftime('%H:%M:%S'))
+    start_boot()
+    print('Bot finalizado às: ', dt.datetime.now().strftime('%H:%M:%S'))
